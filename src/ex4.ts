b6557e5e453f0fe7f9e4f301fb7ac498a44467a3
Beccatoni <rbccmtni1@gmail.com>
2024-07-08 13:14:06 +0200

    type inferring

---
+++ src/ex4.ts
@@ -0,0 +1,8 @@
+/**
+ * 5. Write a TypeScript program that declares a variable without 
+ * specifying its type and shows how TypeScript infers the type 
+ * based on the assigned value.  
+ */
+
+let myVar = 'I am infered to a string type variable'
+
